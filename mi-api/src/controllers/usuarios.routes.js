ab2c8f1e55a2f181fad50b0fb6e0a06a4c49f2ba
Alejandro <Alejandroh.guzman05@gmail.com>
2026-09-07 23:01:53 -0500

    Cambios herran

--- mi-api/src/controllers/usuarios.routes.js
+++ mi-api/src/controllers/usuarios.routes.js
@@ -1,41 +1,35 @@
 const { usuarios } = require('../models/usuarios.models');
 
 exports.register = (req, res) => {
-  // TODO: implementar register
   res.status(501).json({ message: 'Not implemented: register' });
 };
 
 exports.login = (req, res) => {
-  // TODO: implementar login
   res.status(501).json({ message: 'Not implemented: login' });
 };
 
 exports.logout = (req, res) => {
-  // TODO: implementar logout
   res.status(501).json({ message: 'Not implemented: logout' });
 };
 
 exports.forgotPassword = (req, res) => {
-  // TODO: implementar forgotPassword
+
   res.status(501).json({ message: 'Not implemented: forgotPassword' });
 };
 
 exports.resetPassword = (req, res) => {
-  // TODO: implementar resetPassword
   res.status(501).json({ message: 'Not implemented: resetPassword' });
 };
 
 exports.getPerfil = (req, res) => {
-  // TODO: implementar getPerfil
+
   res.status(501).json({ message: 'Not implemented: getPerfil' });
 };
 
 exports.updatePerfil = (req, res) => {
-  // TODO: implementar updatePerfil
   res.status(501).json({ message: 'Not implemented: updatePerfil' });
 };
 
 exports.updatePreferenciasNotificacion = (req, res) => {
-  // TODO: implementar updatePreferenciasNotificacion
   res.status(501).json({ message: 'Not implemented: updatePreferenciasNotificacion' });
 };
