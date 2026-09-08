6d36e10ce2f921697690453a4ef866c230ad8b79
Andres <gerardo.salazar@uniminuto.edu.co>
2026-09-08 09:11:09 -0500

    cambios

--- mi-api/src/app.js
+++ mi-api/src/app.js
@@ -3,3 +3,6 @@ app.use('/api/usuarios', usuariosRoutes);
 
 const parcelaRoutes = require('./routes/parcelaRoutes');
 app.use('/parcelas', parcelaRoutes);
+
+const prediccionroutes= require("./routes/prediccion.routes")
+app.use("/prediccion", prediccionroutes);
