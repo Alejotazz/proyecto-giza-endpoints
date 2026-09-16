d8ec066d7fcb062036105ad865d3dadd36f954c3
Andres <gerardo.salazar@uniminuto.edu.co>
2026-09-15 21:39:46 -0500

    quite los middlware

--- mi-api/src/routes/prediccion.routes.js
+++ mi-api/src/routes/prediccion.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const upload = require("../middlewares/upload.middleware");
+
 const parcelaDatosController = require("../controllers/parcela-datos.controller");
 
 // POST /parcelas/:id/imagenes  (form-data, campo "imagen")
